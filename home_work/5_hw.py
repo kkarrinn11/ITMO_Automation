2eeac7c06fcdcafc9174368bb0b8432bb6fc9409
Karina_M <karina_karina2005@icloud.com>
2025-04-14 22:47:13 +0300

    Доработка

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get ('https://www.saucedemo.com/')
+def check_elements():
+    driver = webdriver.Chrome()
+    driver.get ('https://www.saucedemo.com/')
 
 
-elements = driver.find_element (By.CSS_SELECTOR, '#user-name, #password, .submit-button')
-if elements is None:
-    print('Элементы не найдены')
-else:
-    print('Элементы найдены')
+    elements = driver.find_elements (By.CSS_SELECTOR, '#user-name, #password, #login-button')
+    if elements is None:
+        print('Элементы не найдены')
+    else:
+        print('Элементы найдены')
 
-
+check_elements()
 
